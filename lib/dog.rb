90c52e2919b2647968d39714b35e0527cc15c660
Salem Asrat <salemasrat@gmail.com>
2019-11-20 18:22:04 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,9 @@
 class Dog
-  # code goes here	  attr_reader :name
+  attr_reader :name
   attr_accessor :mood
 
   def initialize(name, mood = "nervous")
     @mood = mood
     @name = name
   end
-end 	end
+end 
