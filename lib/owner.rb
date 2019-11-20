d9bcd9c57311b06a1202a255779b1ef6f49ca2c0
Salem Asrat <salemasrat@gmail.com>
2019-11-20 18:23:24 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -58,8 +58,11 @@ class Owner
     pets.clear
   end
 
-  def list_pets
-   #{pets[:dogs].size} dog(s), and #{pets[:cats].size} cat(s)."
+    def list_pets
+    "I have #{pets[:fishes].size} fish, #{pets[:dogs].size} dog(s), and #{pets[:cats].size} cat(s)."
   end
+
+end
+
 
 end
